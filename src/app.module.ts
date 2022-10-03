34be2fa13b17f7ba03b99d1618dd258c243601ea
Jardel Mitermayer <jardelska@gmail.com>
2022-10-03 17:02:59 -0300

    feat: add mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,30 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CreateUserController } from './create-user/create-user.controller';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     BullModule.forRoot({
       redis: {
         host: 'localhost',
         port: 6379,
       },
     }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT),
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS
+        }
+      }
+    })
   ],
   controllers: [AppController, CreateUserController],
   providers: [AppService],
--- src/create-user/create-user.controller.ts
+++ src/create-user/create-user.controller.ts
@@ -1,10 +1,19 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDTO } from './create-user-dto';
 
 @Controller('create-user')
 export class CreateUserController {
+  constructor(private mailService: MailerService) { }
+
   @Post('/')
   createUser(@Body() createUser: CreateUserDTO) {
+    this.mailService.sendMail({
+      to: createUser.email,
+      from: "Equipe Code/Drops <codedrops@codedrops.com.br>",
+      subject: "Seja bem vindo(a)!",
+      text: `Olá ${createUser.name}, seu cadastro foi realizado com sucesso. Seja bem vindo(a)!`
+    })
     return createUser;
   }
 }
